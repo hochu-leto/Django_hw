58a3d5bf7cd10127e8ab7170f5020eeaf7b93f74
Тимофей <porno_mail@bk.ru>
2021-11-09 00:49:17 +0300

    всё работает

--- databases_2/m2m-relations/articles/models.py
+++ databases_2/m2m-relations/articles/models.py
@@ -1,24 +1,7 @@
 from django.db import models
 
-
-class Article(models.Model):
-
-    title = models.CharField(max_length=256, verbose_name='Название')
-    text = models.TextField(verbose_name='Текст')
-    published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-
-    class Meta:
-        verbose_name = 'Статья'
-        verbose_name_plural = 'Статьи'
-
-    def __str__(self):
-        return self.title
-
-
 class Scope(models.Model):
     name = models.CharField(max_length=50, verbose_name='Имя')
-    articles = models.ManyToManyField(Article, through='Relationship')
 
     class Meta:
         verbose_name = 'Тег'
@@ -28,9 +11,25 @@ class Scope(models.Model):
         return self.name
 
 
+class Article(models.Model):
+
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    scope = models.ManyToManyField(Scope, through='Relationship')
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+    def __str__(self):
+        return self.title
+
+
 class Relationship(models.Model):
     scope = models.ForeignKey(Scope, on_delete=models.CASCADE, verbose_name='Теги')
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
     is_main = models.BooleanField(verbose_name='Основной тег', default=False)
 
     class Meta:
--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -3,7 +3,7 @@ from pprint import pprint
 from django.views.generic import ListView
 from django.shortcuts import render
 
-from articles.models import Article, Relationship
+from articles.models import Article, Relationship, Scope
 
 
 def articles_list(request):
@@ -11,7 +11,6 @@ def articles_list(request):
     context = {
         'object_list': Article.objects.all()
     }
-    pprint(Article.objects.all()[1].scope)#.prefetch_related('scopes'))
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
